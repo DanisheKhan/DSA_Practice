540f477b5cee5da1c342ce801f4f9536f80ee749
DanisheKhan <khandanishe125@gmail.com>
2026-02-10 15:19:10 +0530

    Fix hollow rectangle pattern logic and add new Inverted and Rotated Triangle class

--- A13_HollowRectangle.java
+++ A13_HollowRectangle.java
@@ -1,8 +1,8 @@
 public class A13_HollowRectangle {
   public static void HollowRectangle(int row, int col) {
-    for (int i = 0; i <= row; i++) {
-      for (int j = 0; j <= col; j++) {
-        if (i == 0 || i == row || j == 0 || j == col) {
+    for (int i = 1; i <= row; i++) {
+      for (int j = 1; j <= col; j++) {
+        if (i == 1 || i == row || j == 1 || j == col) {
           System.out.print("*");
         }else{
           System.out.print(" ");
---
+++ A14_InvertedAndRotatedTriangle.java
@@ -0,0 +1,5 @@
+public class A14_InvertedAndRotatedTriangle {
+  public static void main(String[] args) {
+    
+  }
+}

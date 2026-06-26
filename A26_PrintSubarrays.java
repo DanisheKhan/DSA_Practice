0664c133162da877b935d0a2fc6de643a401b1a4
DanisheKhan <khandanishe125@gmail.com>
2026-06-27 01:35:40 +0530

    refactor: simplify subarray printing logic and add new program to calculate subarray sums

--- A26_PrintSubarrays.java
+++ A26_PrintSubarrays.java
@@ -1,43 +1,14 @@
 public class A26_PrintSubarrays {
-    // Print all possible subarrays and calculate their sums
-    public static void printSubarrays(int[] numbers) {
-        int totalSubarrays = 0;
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-
-        for (int i = 0; i < numbers.length; i++) {
-            int start = i;
-            for (int j = i; j < numbers.length; j++) {
-                int end = j;
-                int sum = 0;
-                
-                System.out.print("[");
-                for (int k = start; k <= end; k++) {
-                    System.out.print(numbers[k]);
-                    if (k < end) {
-                        System.out.print(", ");
-                    }
-                    sum += numbers[k];
-                }
-                System.out.print("] (Sum = " + sum + ")\n");
-                totalSubarrays++;
-
-                if (sum > max) {
-                    max = sum;
-                }
-                if (sum < min) {
-                    min = sum;
-                }
-            }
-            System.out.println();
-        }
-        System.out.println("Total Subarrays: " + totalSubarrays);
-        System.out.println("Max Subarray Sum: " + max);
-        System.out.println("Min Subarray Sum: " + min);
-    }
-
     public static void main(String[] args) {
-        int[] numbers = {2, 4, 6, 8, 10};
-        printSubarrays(numbers);
+        int[] arr = { 1, 2, 3, 4 };
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k]+" ");
+                }
+                System.out.println();
+            }
+        }
     }
 }
---
+++ A27_SumOfSubArray.java
@@ -0,0 +1,16 @@
+public class A27_SumOfSubArray {
+  public static void main(String[] args) {
+    int[] arr = { 1, 2, 3, 4 };
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i; j < arr.length; j++) {
+        int sum = 0;
+        for (int k = i; k <= j; k++) {
+          System.out.print(arr[k]+" ");
+          sum += arr[k];
+        }
+        System.out.print(" Sum is : " + sum);
+        System.out.println();
+      }
+    }
+  }
+}

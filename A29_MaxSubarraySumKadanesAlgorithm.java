cd1759fcbe08cee57337ba19e697f923c002c837
DanisheKhan <khandanishe125@gmail.com>
2026-07-06 16:09:42 +0530

    feat: implement Kadane's algorithm for maximum subarray sum with test case in main

--- A29_MaxSubarraySumKadanesAlgorithm.java
+++ A29_MaxSubarraySumKadanesAlgorithm.java
@@ -1,5 +1,20 @@
 public class A29_MaxSubarraySumKadanesAlgorithm {
+
+  public static int MaxSubarraySumKadanes(int[] arr) {
+    int maximum = Integer.MIN_VALUE;
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      sum += arr[i];
+      maximum = Math.max(maximum, sum);
+      if (sum < 0) {
+        sum = 0;
+      }
+    }
+    return maximum;
+  }
+
   public static void main(String[] args) {
-    
+    int[] arr = { 5, 4, -1, 7, 8 };
+    System.out.println(MaxSubarraySumKadanes(arr));
   }
 }

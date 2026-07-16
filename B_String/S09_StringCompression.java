8d285e6dacd515800db4dc772efe9f663d246fee
DanisheKhan <khandanishe125@gmail.com>
2026-07-16 15:59:54 +0530

    feat: add StringCompression utility to compress repeating characters in a string

---
+++ B_String/S09_StringCompression.java
@@ -0,0 +1,23 @@
+public class S09_StringCompression {
+  public static String Compress(String str) {
+    StringBuilder sb = new StringBuilder("");
+    for (int i = 0; i < str.length(); i++) {
+      Integer count = 1;
+      while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+        i++;
+        count++;
+      }
+      sb.append(str.charAt(i));
+
+      if (1 < count) {
+        sb.append(count.toString());
+      }
+    }
+    return sb.toString();
+  }
+
+  public static void main(String[] args) {
+    String str="aaabbbccc";
+    System.out.println(Compress(str));
+  }
+}
